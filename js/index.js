764c836cbeac2db9d55b91bad5921fbfd4931a8b
zhangkai0621 <45391737+zhangkai0621@users.noreply.github.com>
2019-01-09 23:18:52 +0800

    主页电商学院Coding

--- js/index.js
+++ js/index.js
@@ -2,3 +2,22 @@
 $(function(){
     $("#header").load("header.html");
 })
+
+//解决方案
+    var serImgs=document.querySelectorAll(".serImg");  
+    var checFatherDns=document.querySelectorAll(".checFatherDn"); 
+    for(var checFatherDn of checFatherDns){
+        checFatherDn.onmouseover=function(){
+            var checFatherDn=this;
+            var serImg=checFatherDn.firstElementChild;
+            serImg.className="serChecked"; 
+            checFatherDn.className="checFather";
+        }
+        checFatherDn.onmouseout=function(){
+            var checFatherDn=this;
+            var serImg=checFatherDn.firstElementChild;
+            serImg.className="serImg";
+            checFatherDn.className="checFatherDn";
+        }
+    }
+
