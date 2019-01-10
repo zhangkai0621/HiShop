1d94b0c38260e70b7c3c7eea03f790d32a673010
ZhangKai <1244234523@qq.com>
2019-01-10 21:08:41 +0800

    主页banner轮播效果Coding

--- js/index.js
+++ js/index.js
@@ -2,9 +2,10 @@
 $(function(){
     $("#header").load("header.html");
     $("#footer").load("footer.html");
-})
+});
 
 //解决方案
+(function(){
     var serImgs=document.querySelectorAll(".serImg");  
     var checFatherDns=document.querySelectorAll(".checFatherDn"); 
     for(var checFatherDn of checFatherDns){
@@ -21,4 +22,25 @@ $(function(){
             checFatherDn.className="checFatherDn";
         }
     }
+})();
+//banner轮播图
+    (function(){
+       var banner=document.getElementsByClassName("banner")[0];
+       console.log(banner)
+       var i=1;
+       function change(){
+            if(i==6) i=1; 
+            if(i<6){
+                i++;
+                console.log(i);
+                banner.style.background=`url(img/banner${i}.jpg) no-repeat`;
+                banner.className="banner banChange";   
+            }
+            //banner.className="banner";  
+       }
+       var clear=setInterval(change,3000);
+    //    banner.onmouseover=function(){
+    //        clearInterval(clear);
+    //    }
+    })();
 
